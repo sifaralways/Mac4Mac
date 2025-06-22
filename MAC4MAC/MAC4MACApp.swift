83c7c49bc8bd3e23f49b0b86c452be8b8ee2c23d
Akshat Singhal <61392112+sifaralways@users.noreply.github.com>
2025-06-22 22:58:59 +1000

    Initial Commit

---
+++ MAC4MAC/MAC4MACApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAC4MACApp.swift
+//  MAC4MAC
+//
+//  Created by Akshat Singhal on 22/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct MAC4MACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
